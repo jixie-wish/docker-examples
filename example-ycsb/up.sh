46df61da79e87156c382c5111eda711f1be4c1a4
jixie <jixie>
2024-02-05 14:25:07 +0800

    init mongodb examples

--- example-ycsb/up.sh
+++ example-ycsb/up.sh
@@ -3,13 +3,12 @@
 docker compose build --no-cache
 
 docker compose up --no-start
-docker compose start crdb-0
-docker compose start crdb-1
-docker compose start crdb-2
-docker compose start lb
-docker compose start crdb-init
-
-sleep 5
+docker compose start mongodb-0
+sleep 20
+docker compose start mongodb-1
+sleep 20
+docker compose start mongodb-2
+sleep 20
 
 echo "starting ycsb..."
 docker compose start ycsb
